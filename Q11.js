e4a712ed7e0ac29f461c7661e4410513b6b6d9ec
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-03-04 21:24:10 -0800

    Q-11 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignments Q-11

---
+++ Q11.js
@@ -0,0 +1,14 @@
+"use strict";
+// 11.	Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let names = ["Ambreen", "Ramsha", "Tehreem", "Zoya"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+// 2nd example
+// Define an array of names
+let fNames = ["Alice", "Bob", "Charlie", "David", "Eve"];
+// Print each person's name by accessing each element in the list, one at a time
+for (let i = 0; i < fNames.length; i++) {
+    console.log(fNames[i]);
+}
---
+++ Q11.ts
@@ -0,0 +1,19 @@
+// 11.	Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let names: string[] = ["Ambreen" , "Ramsha" , "Tehreem" , "Zoya"]
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
+// 2nd way
+// Define an array of names
+let fNames: string[] = ["Alice", "Bob", "Charlie", "David", "Eve"];
+
+// Print each person's name by accessing each element in the list, one at a time
+for (let i = 0; i < fNames.length; i++) {
+    console.log(fNames[i]);
+}
+
+
+
